f42257469fd557d5d2b1537e5e25504a48e49a5f
Your Name <your_email@example.com>
2024-04-13 17:36:36 +0200

    Added untracked files and folders

---
+++ app.py
@@ -0,0 +1,14 @@
+from flask import Flask, render_template
+import requests
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    epg_data = requests.get('https://github.com/GreekTVApp/epg-greece-cyprus/releases/download/EPG/epg.xml').text
+    playlist_data = requests.get('https://raw.githubusercontent.com/gluk03/iptvgluk/dd9409c9f9029f6444633267e3031741efedc381/TV.m3u').text
+    external_content = requests.get('https://foothubhd.xyz/').text
+    return render_template('index.html', epg_data=epg_data, playlist_data=playlist_data, external_content=external_content)
+
+if __name__ == '__main__':
+    app.run(debug=True)
