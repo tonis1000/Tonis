74b40b7ce82dde5bf6de9e0e5a3bc621058049b8
tonis1000 <atonis@live.com>
2024-04-13 18:28:18 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
-from flask import Flask, render_template
-import requests
+from flask import Flask, render_template, send_from_directory
 
 app = Flask(__name__)
 
@@ -9,6 +8,10 @@ def index():
     playlist_data = requests.get('https://raw.githubusercontent.com/gluk03/iptvgluk/dd9409c9f9029f6444633267e3031741efedc381/TV.m3u').text
     external_content = requests.get('https://foothubhd.xyz/').text
     return render_template('index.html', epg_data=epg_data, playlist_data=playlist_data, external_content=external_content)
+
+@app.route('/playlist.m3u')
+def serve_playlist():
+    return send_from_directory('static', 'playlist.m3u')
 
 if __name__ == '__main__':
     app.run(debug=True)
