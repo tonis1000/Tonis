f37800e94bf8113bde585fa8f33ebad0566f6a85
tonis1000 <atonis@live.com>
2024-04-14 17:36:00 +0200

    Update download_xml.py

--- download_xml.py
+++ download_xml.py
@@ -1,31 +1,22 @@
 import requests
 
-# URL der XML-Datei
-url = 'https://github.com/GreekTVApp/epg-greece-cyprus/releases/download/EPG/epg.xml'
-
-# Pfad, unter dem die XML-Datei gespeichert werden soll
-local_path = 'data/epg.xml'
-
 def download_xml(url, local_path):
-    try:
-        # Herunterladen der XML-Datei
-        response = requests.get(url)
+    # Herunterladen der XML-Datei
+    response = requests.get(url)
 
-        # Überprüfen, ob der Download erfolgreich war
-        if response.status_code == 200:
-            # Speichern der XML-Datei im lokalen Ordner
-            with open(local_path, 'wb') as f:
-                f.write(response.content)
-            print("XML-Datei erfolgreich heruntergeladen und gespeichert.")
-        else:
-            print("Fehler beim Herunterladen der XML-Datei. Statuscode:", response.status_code)
-            with open('download_log.txt', 'a') as log_file:
-                log_file.write("Fehler beim Herunterladen der XML-Datei. Statuscode:" + str(response.status_code) + "\n")
-    except Exception as e:
-        print("Fehler beim Herunterladen der XML-Datei:", str(e))
-        with open('download_log.txt', 'a') as log_file:
-            log_file.write("Fehler beim Herunterladen der XML-Datei:" + str(e) + "\n")
+    # Überprüfen, ob der Download erfolgreich war
+    if response.status_code == 200:
+        # Speichern der XML-Datei im lokalen Ordner
+        with open(local_path, 'wb') as f:
+            f.write(response.content)
+        print("XML-Datei erfolgreich heruntergeladen und gespeichert.")
+    else:
+        print("Fehler beim Herunterladen der XML-Datei.")
 
 if __name__ == '__main__':
+    # URL der XML-Datei
+    url = 'https://github.com/GreekTVApp/epg-greece-cyprus/releases/download/EPG/epg.xml'
+    # Pfad, unter dem die XML-Datei gespeichert werden soll
+    local_path = 'data/epg.xml'
     # Funktion aufrufen, um die XML-Datei herunterzuladen und zu speichern
     download_xml(url, local_path)
