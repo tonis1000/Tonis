e1f5eddb1dfdbbfa4ce9effcb94fe7992a41490d
tonis1000 <atonis@live.com>
2024-04-14 23:13:34 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
-from flask import Flask, render_template, jsonify
-import xml.etree.ElementTree as ET
+from flask import Flask, render_template, jsonify, send_from_directory
+import os
 import requests
 
 app = Flask(__name__)
@@ -17,34 +17,17 @@ def download_xml(url, local_path):
     else:
         print("Fehler beim Herunterladen der XML-Datei.")
 
-def parse_epg(xml_data):
-    programs = []
-    root = ET.fromstring(xml_data)
-    channels = {channel.get('id'): channel.find('display-name').text for channel in root.findall('.//channel')}
-    for programme in root.findall('.//programme'):
-        channel_id = programme.get('channel')
-        channel_name = channels.get(channel_id, 'Unknown Channel')
-        program_title = programme.find('title').text
-        programs.append((channel_name, program_title))
-    return programs
-
 @app.route('/')
 def index():
-    # Heruntergeladene XML-Datei verwenden
-    download_xml('https://github.com/GreekTVApp/epg-greece-cyprus/releases/download/EPG/epg.xml', 'data/epg.xml')
     with open('data/epg.xml', 'r') as f:
         epg_data = f.read()
-    programs = parse_epg(epg_data)
-    return render_template('index.html', programs=programs)
+    return render_template('index.html')
 
 @app.route('/reload-epg')
 def reload_epg():
-    # Herunterladen und Parsen der XML-Datei
+    # Herunterladen und Speichern der XML-Datei
     download_xml('https://github.com/GreekTVApp/epg-greece-cyprus/releases/download/EPG/epg.xml', 'data/epg.xml')
-    with open('data/epg.xml', 'r') as f:
-        epg_data = f.read()
-    # Rückgabe der EPG-Daten im XML-Format
-    return epg_data
+    return send_from_directory(os.path.join(app.root_path, 'data'), 'epg.xml')
 
 if __name__ == '__main__':
     app.run(debug=True)
