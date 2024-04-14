f5fee820b987b23026dc7c674ad51c40f3cd544e
tonis1000 <atonis@live.com>
2024-04-14 18:03:23 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 # app.py
 
-from flask import Flask, render_template, send_from_directory, request
+from flask import Flask, render_template
 import xml.etree.ElementTree as ET
 from download_xml import download_xml
 
@@ -23,14 +23,8 @@ def index():
     download_xml('https://github.com/GreekTVApp/epg-greece-cyprus/releases/download/EPG/epg.xml', 'data/epg.xml')
     with open('data/epg.xml', 'r') as f:
         epg_data = f.read()
-    playlist_data = requests.get('https://raw.githubusercontent.com/gluk03/iptvgluk/dd9409c9f9029f6444633267e3031741efedc381/TV.m3u').text
-    external_content = requests.get('https://foothubhd.xyz/').text
     programs = parse_epg(epg_data)
-    return render_template('index.html', epg_data=epg_data, playlist_data=playlist_data, external_content=external_content, programs=programs)
-
-@app.route('/playlist.m3u')
-def serve_playlist():
-    return send_from_directory('static', 'playlist.m3u')
+    return render_template('index.html', programs=programs)
 
 if __name__ == '__main__':
     app.run(debug=True)
