0907b9b482d314235a7cf4ae7f882e3352a2a2a4
tonis1000 <atonis@live.com>
2024-05-19 13:52:38 +0200

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,15 +1,29 @@
+// Funktion zum Ausführen einer Proxy-Anfrage im Browser
+async function makeProxyRequest(url) {
+    try {
+        const response = await fetch(url); // Fetch-API verwenden, um die Anfrage zu senden
+        if (!response.ok) {
+            throw new Error('Network response was not ok');
+        }
+        const responseData = await response.text(); // Response-Text abrufen
+        console.log('Response data:', responseData); // Ausgabe des Response-Texts zur Überprüfung
+        return responseData; // Die Daten zurückgeben
+    } catch (error) {
+        console.error('Proxy request failed:', error);
+        throw error; // Fehler weiterleiten
+    }
+}
+
 // Funktion zum Laden der Playlist.m3u und Aktualisieren der Sidebar
 function loadMyPlaylist() {
-    fetch('playlist.m3u')
-        .then(response => response.text())
+    makeProxyRequest('playlist.m3u')
         .then(data => updateSidebarFromM3U(data))
         .catch(error => console.error('Fehler beim Laden der Playlist:', error));
 }
 
 // Funktion zum Laden der externen Playlist und Aktualisieren der Sidebar
 function loadExternalPlaylist() {
-    fetch('https://raw.githubusercontent.com/gluk03/iptvgluk/dd9409c9f9029f6444633267e3031741efedc381/TV.m3u')
-        .then(response => response.text())
+    makeProxyRequest('https://raw.githubusercontent.com/gluk03/iptvgluk/dd9409c9f9029f6444633267e3031741efedc381/TV.m3u')
         .then(data => updateSidebarFromM3U(data))
         .catch(error => console.error('Fehler beim Laden der externen Playlist:', error));
 }
