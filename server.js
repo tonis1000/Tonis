aceecf93d4c8f486439fec8a45db0f85cc0c0dc4
Your Name <your_email@example.com>
2024-06-25 23:16:52 +0200

    Updated server.js with POST routes for saving and deleting URLs

--- server.js
+++ server.js
@@ -7,29 +7,40 @@ app.use(express.json());
 
 app.post('/save-url', (req, res) => {
     const url = req.body.url;
+    if (!url) {
+        return res.status(400).send('URL fehlt');
+    }
+    
     fs.appendFile('urls.txt', url + '\n', (err) => {
         if (err) {
-            res.status(500).send('Fehler beim Speichern der URL');
-        } else {
-            res.send('URL erfolgreich gespeichert');
+            console.error('Fehler beim Speichern der URL:', err);
+            return res.status(500).send('Fehler beim Speichern der URL');
         }
+        console.log('URL erfolgreich gespeichert:', url);
+        res.send('URL erfolgreich gespeichert');
     });
 });
 
 app.post('/delete-url', (req, res) => {
     const url = req.body.url;
+    if (!url) {
+        return res.status(400).send('URL fehlt');
+    }
+    
     fs.readFile('urls.txt', 'utf8', (err, data) => {
         if (err) {
-            res.status(500).send('Fehler beim Lesen der Datei');
-            return;
+            console.error('Fehler beim Lesen der Datei:', err);
+            return res.status(500).send('Fehler beim Lesen der Datei');
         }
+        
         const urls = data.split('\n').filter(u => u !== url);
         fs.writeFile('urls.txt', urls.join('\n'), (err) => {
             if (err) {
-                res.status(500).send('Fehler beim Löschen der URL');
-            } else {
-                res.send('URL erfolgreich gelöscht');
+                console.error('Fehler beim Löschen der URL:', err);
+                return res.status(500).send('Fehler beim Löschen der URL');
             }
+            console.log('URL erfolgreich gelöscht:', url);
+            res.send('URL erfolgreich gelöscht');
         });
     });
 });
