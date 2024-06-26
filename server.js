ed6b9f12b4d89bed62a7c6bd1c037c8bf95b48b6
tonis1000 <atonis@live.com>
2024-06-26 07:07:41 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const fs = require('fs');
+const path = require('path');
 const app = express();
 const port = 3000;
 
+// Middleware to serve static files
+app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.json());
 
-// Eine Beispiel-Route für die Startseite
 app.get('/', (req, res) => {
     res.send('Hello World!');
 });
