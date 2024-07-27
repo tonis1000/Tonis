f0569acfe566fe9042d5dacac584ee17e3df641a
tonis1000 <atonis@live.com>
2024-07-27 13:22:07 +0200

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -81,6 +81,22 @@ document.getElementById('copy-button').addEventListener('click', function() {
 });
 
 
+var video = document.getElementById('video-player');
+var videoSrc = 'your-stream-url.m3u8'; // Setze hier deine URL ein
+
+if (Hls.isSupported()) {
+    var hls = new Hls();
+    hls.loadSource(videoSrc);
+    hls.attachMedia(video);
+    hls.on(Hls.Events.MANIFEST_PARSED, function() {
+        video.play();
+    });
+} else if (video.canPlayType('application/vnd.apple.mpegurl')) {
+    video.src = videoSrc;
+    video.addEventListener('loadedmetadata', function() {
+        video.play();
+    });
+}
 
 
 
